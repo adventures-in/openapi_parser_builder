ffdd3298404ce934d0af9cc6885cc8d8a9191965
Nick Meinhold <nick.meinhold@gmail.com>
2021-03-28 21:57:46 +1100

    Separate schema builder & clean up build.yaml

--- lib/openapi_client_builder.dart
+++ lib/openapi_client_builder.dart
@@ -5,8 +5,5 @@ library openapi_client_builder;
 
 import 'package:build/build.dart';
 import 'package:openapi_client_builder/src/builders/client_builder.dart';
-import 'package:openapi_client_builder/src/builders/schema_builder.dart';
-
-Builder schemaBuilder(BuilderOptions options) => SchemaBuilder();
 
 Builder clientBuilder(BuilderOptions options) => ClientBuilder();
---
+++ lib/openapi_schema_builder.dart
@@ -0,0 +1,9 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+// library openapi_client_builder;
+
+import 'package:build/build.dart';
+import 'package:openapi_client_builder/src/builders/schema_builder.dart';
+
+Builder schemaBuilder(BuilderOptions options) => SchemaBuilder();
